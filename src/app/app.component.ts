6afef54f9257fe1b4f3aa89f935a18b50a316b9c
Mahmoud Hussein Tayem <68419183+mahmoudhtayem@users.noreply.github.com>
2021-05-17 17:28:09 +0400

    Use Current Location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,73 +4,69 @@ import { HttpClient } from '@angular/common/http';
 declare const Liferay: any;
 
 @Component({
-	templateUrl: 
-		Liferay.ThemeDisplay.getPathContext() + 
+	templateUrl:
+		Liferay.ThemeDisplay.getPathContext() +
 		'/o/Weather-Widget/app/app.component.html'
 })
 export class AppComponent implements AfterViewInit {
 	params: LiferayParams;
 	labels: any;
-	public WeatherData : any = null;
-	constructor(public http:HttpClient) {
-		this.labels = {  
+	public WeatherData: any = null;
+	constructor(public http: HttpClient) {
+		this.labels = {
 			configuration: Liferay.Language.get('configuration'),
 			portletNamespace: Liferay.Language.get('portlet-namespace'),
-        	contextPath: Liferay.Language.get('context-path'),
+			contextPath: Liferay.Language.get('context-path'),
 			portletElementId: Liferay.Language.get('portlet-element-id'),
 		}
-		
+
 	}
 	get configurationJSON() {
 		return JSON.stringify(this.params.configuration, null, 2);
 	}
-	get APIKey()
-	{
+	get APIKey() {
 		return this.params.configuration.system["APIKey"];
 	}
-	get Location()
-	{
+	get Location() {
 		return this.params.configuration.portletInstance.Location;
 	}
-	
-	get UseCurrentLocation()
-	{
-		return this.params.configuration.portletInstance["UseCurrentLocation"] as boolean;
+
+	get UseCurrentLocation() {
+		return this.params.configuration.portletInstance["UseCurrentLocation"];
 	}
-	public getCurrentWeatherData()
-	{
+	public getWeatherData() {
+		console.log("getCurrentWeatherData");
 		var key = this.APIKey;
 		var location = this.Location;
 		this.http
-		.get("http://api.weatherapi.com/v1/current.json?key="+key+"&q="+location+"&aqi=yes")
-		.subscribe(result=>{
-			this.WeatherData = result;
-		});
+			.get("http://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + location + "&aqi=yes")
+			.subscribe(result => {
+				this.WeatherData = result;
+			});
 	}
-	public getCurrentWeatherDataByLatLNG(Region:any)
-	{
+	public getCurrentWeatherDataByLatLNG(Region: any) {
 		var key = this.APIKey;
 		var location = this.Location;
 		this.http
-		.get("http://api.weatherapi.com/v1/current.json?key="+key+"&q="+Region+"&aqi=yes")
-		.subscribe(result=>{
-			this.WeatherData = result;
-		});
+			.get("http://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + Region + "&aqi=yes")
+			.subscribe(result => {
+				this.WeatherData = result;
+			});
 	}
 	public ngAfterViewInit() {
 		
-		if(!this.UseCurrentLocation)
-		this.getCurrentWeatherData();
+		if (this.UseCurrentLocation == "false") {
+			
+			this.getWeatherData();
+		}
 		else
-		this.getCurrentLocation();
+			this.getCurrentLocation();
 	}
-	public getCurrentLocation()
-	{
-
+	public getCurrentLocation() {
 		this.http.get("http://ip-api.com/json/?fields=status,message,countryCode,region,regionName")
-		.subscribe(result=>{
-			console.log(result);
-			this.getCurrentWeatherDataByLatLNG(result["regionName"]);
-		});
+			.subscribe(result => {
+				console.log(result);
+				this.getCurrentWeatherDataByLatLNG(result["regionName"]);
+			});
 	}
 }
